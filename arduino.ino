4f31cbc1b6001ab913732cc9cddea12c3fc327ed
Raygunito <112569656+Raygunito@users.noreply.github.com>
2024-01-31 21:20:07 +0100

    feat: rotation message treated
    we should now be capable of retrieve the left and right side value for our dc motor

--- arduino.ino
+++ arduino.ino
@@ -2,19 +2,28 @@
 #include <WiFiNINA.h>
 #include <Wire.h>
 #include "Grove_I2C_Motor_Driver.h"
-#define I2C_ADDRESS 0x0F
+
+// use I2C Scanner to find address of both Motor driver...
+// right now we will use Left and Right motor side
+#define I2C_FRONT_ADDRESS 0x08
+#define I2C_REAR_ADDRESS 0x0A
 
 char ssid[] = "ArduinoMotor";
 char pass[] = "12345678";
 int status = WL_IDLE_STATUS;
-
 WiFiServer server(8888);
+
+const char delimiter = ',';
+
+I2CMotorDriver frontDriver;
+I2CMotorDriver rearDriver;
 
 void setup()
 {
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
-    Motor.begin(I2C_ADDRESS);
+    frontDriver.begin(I2C_FRONT_ADDRESS);
+    rearDriver.begin(I2C_REAR_ADDRESS);
     startServer();
 }
 
@@ -29,9 +38,22 @@ void loop()
         {
             if (client.available())
             {
-                int command = atoi(client.readStringUntil('\n').c_str());
-                Serial.println(command);
-                processCommand(command);
+                const char *message = client.readStringUntil('\n').c_str();
+                char *token = strtok(const_cast<char *>(message), &delimiter);
+                if (token != NULL)
+                {
+                    int value1 = atoi(token);
+                    token = strtok(NULL, ",");
+
+                    if (token != NULL)
+                    {
+                        int value2 = atoi(token);
+                        Serial.println("Received values: V1 and V2");
+                        Serial.println(value1);
+                        Serial.println(value2);
+                        processCommand(value1, value2);
+                    }
+                }
             }
         }
         client.stop();
@@ -76,21 +98,32 @@ void startServer()
     Serial.println(ip);
 }
 
-void processCommand(int motorValue)
+void processCommand(int leftMotorValue, int rightMotorValue)
 {
     // Ensure motorValue is within the valid range
-    if (motorValue >= 100)
+    if (leftMotorValue >= 100)
     {
-        motorValue = 100;
+        leftMotorValue = 100;
     }
     else
     {
-        if (motorValue <= -100)
+        if (leftMotorValue <= -100)
         {
-            motorValue = -100;
+            leftMotorValue = -100;
+        }
+    }
+    if (rightMotorValue >= 100)
+    {
+        rightMotorValue = 100;
+    }
+    else
+    {
+        if (rightMotorValue <= -100)
+        {
+            rightMotorValue = -100;
         }
     }
 
-    Motor.speed(MOTOR1, motorValue);
-    Motor.speed(MOTOR2, motorValue);
+    frontDriver.speed(MOTOR1, leftMotorValue);
+    frontDriver.speed(MOTOR2, rightMotorValue);
 }
